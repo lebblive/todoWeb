9dbd16f38efaa4532c0d0bfbb5d0b4f671fa8464
kevin <lebblive@gmail.com>
2020-11-11 19:48:15 +0200

    first add

---
+++ public/js/home.js
@@ -0,0 +1,16 @@
+function test(){
+	alert("marche");
+}
+// // json 
+// var tache1 = {
+// 	name: 'Tondre la pelouse',
+// 	done: false
+// };
+// var tache2 = {
+// 	name: 'Faire la vaisselle',
+// 	done: false
+// };
+// var tache3 = {
+// 	name: 'Ranger le bureau',
+// 	done: true
+// };
